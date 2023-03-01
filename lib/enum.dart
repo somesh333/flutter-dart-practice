06cb143ef1c90314298dad802b63efd440d7bc33
unknown <someshjha.333@gmail.com>
2023-03-01 23:48:32 +0530

    enum

---
+++ lib/enum.dart
@@ -0,0 +1,9 @@
+Switch case:-
+
+enum : a special class use to represent a fixed number of constant value.
+
+eg: wheather condition enum containing all type of all condition
+
+=> enum Condition {Sunny , Rainy , cloudy}
+
+====================================================================
